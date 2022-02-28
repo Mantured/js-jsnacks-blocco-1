cc38a9f6ec6bef749ca2ce23ec1e44579a1949bb
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-02-28 21:06:29 +0100

    some different version of snack 4 and 6 - end

--- jsnack4/main.js
+++ jsnack4/main.js
@@ -12,21 +12,24 @@ const guestes = [
 ];
 console.log(guestes);
 
+document.querySelector('button').addEventListener(`click`, function () {
+    const guest = document.querySelector('input');
 
+    let isInvited = false;
 
-const guest = prompt('nome invitato').toLowerCase().trim();
+    for (let i = 0; i < guestes.length; i++) {
+        if (guestes[i].toLowerCase() === (guest.value).toLowerCase().trim())
+            isInvited = true;
+    }
 
-let isInvited = false;
+    console.log(isInvited);
 
-for (let i = 0; i < guestes.length; i++) {
-    if (guestes[i].toLowerCase() === guest)
-        isInvited = true;
-}
-
-console.log(isInvited);
-
-if (isInvited) {
-    console.log('sei invitato');
-} else {
-    console.log('stai fuori');
-}
+    if (isInvited) {
+        document.getElementById('message').innerHTML=`${(guest.value).toLowerCase().trim()} sei atuorizzato ad entrare, mantieni la distanza di 1m dagli altri ospiti ed indossa la mascherina. Divertiti!`
+        console.log('sei invitato');
+    } else {
+        document.getElementById('message').innerHTML=`${(guest.value).toLowerCase().trim()} Non sei autorizzato ad entrare, prego seguire i genitli signori in nero, ti porteranno all'uscita!`
+        console.log('stai fuori');
+    }
+    guest.value = "";
+})
--- jsnack6/main.js
+++ jsnack6/main.js
@@ -1,10 +1,27 @@
 
-const sevenNumber = prompt('inserisci un numero di 7 cifre');
-let sum = 0;
-console.log(sevenNumber);
 
-for (let i = 0; i < sevenNumber.length; i++) {
-    sum += parseInt(sevenNumber.charAt(i));    
+
+const message = document.getElementById('message');
+
+
+//const sevenNumbers = prompt('inserisci un numero di 7 cifre').split(''); //split version
+let sum = 0;
+
+const sevenNumbers = prompt('inserisci un numero di 7 cifre'); // no split version 
+
+
+console.log(sevenNumbers);
+
+// #versione con il for ()i
+for (let i = 0; i < sevenNumbers.length; i++) {
+    sum += parseInt(sevenNumbers.charAt(i));    
 }
 
+//# versione con sevenNumbers and number come variabili al posto di i 
+
+/* for (let number of sevenNumbers) {
+    sum += parseInt(number);
+} */
+
+message.innerHTML = `Sum is ${sum}`
 console.log(sum);
